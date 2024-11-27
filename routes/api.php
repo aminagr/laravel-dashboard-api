3688a0687de6f9eb5beeec68c3cd6b651a2f435c
aminagr <minatub@gmail.com>
2024-11-27 18:02:14 +0100

    update profile data and password

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
@@ -15,5 +16,8 @@ Route::post('forgot-password', [AuthController::class, 'sendResetLink']);
 Route::post('reset-password', [AuthController::class, 'resetPassword']);
 
 
+
+Route::put('user/update', [UserController::class, 'updateUser'])->middleware('auth:sanctum');
+Route::post('user/change-password', [UserController::class, 'changePassword'])->middleware('auth:sanctum');
 
 Route::apiResource('products', ProductController::class);
